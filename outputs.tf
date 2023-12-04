b83bd5461fff7c65ffe9ea5a284d1a6dec64b37c
scent-y <chewbacca200_walkingc@yahoo.co.jp>
2023-12-04 19:10:24 +0900

    terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -18,10 +18,10 @@ output "website_bucket_arn" {
 
 output "website_bucket_name" {
   description = "Name (id) of the bucket"
-  value = module.website_s3_bucket.name
+  value       = module.website_s3_bucket.name
 }
 
 output "website_bucket_domain" {
   description = "Domain name of the bucket"
-  value = module.website_s3_bucket.domain
+  value       = module.website_s3_bucket.domain
 }
